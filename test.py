7a83371da9c3edba2f1c1fe9a19d7e8d31a14be8
Александр Загорулько <alekszagorulcko@yandex.ru>
2022-03-17 15:28:34 +0300

    reinstall venv

--- test.py
+++ test.py
@@ -1,21 +1,5 @@
-from math import sqrt
+def we_crash_all(name: str) -> str:
+    return 'Привет, ' + name + ', мы всё сломали!'
 
 
-message = ('Добро пожаловать в самую лучшую программу для вычисления' +
-           'квадратного корня из заданного числа')
-
-
-def calculatesquareroot(number):
-    """ Вычисляет квадратный корень"""
-    return sqrt(number)
-
-
-def calc(your_number):
-    if your_number <= 0:
-        return
-    print('Мы вычислили корень квадратный из введенного вами числа.' +
-          f'Это будет: {calculatesquareroot(your_number)}')
-
-
-print(message)
-calc(25.5)
+print(we_crash_all('Наташа'))
